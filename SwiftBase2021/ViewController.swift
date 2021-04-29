ee8a0ad205c423a87733c66500710bc59072634a
LHWen <1109070804@qq.com>
2021-04-29 14:11:06 +0800

    基本语法的使用

--- SwiftBase2021/ViewController.swift
+++ SwiftBase2021/ViewController.swift
@@ -12,8 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        baseMakeSwift()
     }
 
+    // 基本语法
+    func baseMakeSwift() {
+        // var 定义变量， let 定义常量不可变
+        var myString = "Hello, world"
+        myString += "!"
+        print(myString)
+        
+        // 分号使用
+        let letString = "分号表结束，在不换行情况下"; print(letString)
+    }
 
 }
 
